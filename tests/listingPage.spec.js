c5b9b4f7e66ac8999e6040fbf29af28f9d11bcfa
1803amar <amarmishra.1803@gmail.com>
2026-01-28 15:20:10 +0530

    data-IDs updated on listing page automation script

--- tests/listingPage.spec.js
+++ tests/listingPage.spec.js
@@ -14,36 +14,47 @@ test('hackthon listing page', async ({ page }) => {
     }
 
     // to click the flagship on filters section (by using text)
-    // await page.locator("//main//div/div[2]/div[2]//div[4]//div[2]//div/div[2]//div[1]//div/div/div[1]").first().click();
-    await page.getByText('Flagship', { exact: true }).click();
+    await page.locator('[data-id="hackathon-listing-filters-category-flagship"]').click();
+    // await page.getByText('Flagship', { exact: true }).click();
 
     // to click on community on filters section (by using text)
+    await page.locator('[data-id="hackathon-listing-filters-category-community"]').click();
     // await page.getByText('Community', { exact: true }).click();
 
     // to click the free from pricing on filters section (by using text)
-    await page.getByText('Free', { exact: true }).click();
+    await page.locator('[data-id="hackathon-listing-filters-ticket-free"]').click();
+    // await page.getByText('Free', { exact: true }).click();
 
     // to click the paid from pricing on filters section (by using text)
+    await page.locator('[data-id="hackathon-listing-filters-ticket-paid"]').click();
     // await page.getByText('Paid', { exact: true }).click();
 
     // to click the Team from Partipation type on filters section (by using text)
+    await page.locator('[data-id="hackathon-listing-filters-participation-team"]').click();
     // await page.getByText('Team', { exact: true }).click();
 
     // to click the Individual from Partipation type on filters section (by using text)
-    await page.getByText('Individual', { exact: true }).click();
+    await page.locator('[data-id="hackathon-listing-filters-participation-individual"]').click();
+    // await page.getByText('Individual', { exact: true }).click();
 
     // to click the Ongoing Registration from Filter by Activity on filters section (by using text)
-    await page.getByText('Ongoing Registration', { exact: true }).click();
+    await page.locator('[data-id="hackathon-listing-filters-module-ongoing-registration"]').click();
+    // await page.getByText('Ongoing Registration', { exact: true }).click();
 
     // to click the Ongoing Submission from Filter by Activity on filters section (by using text)
+    await page.locator('[data-id="hackathon-listing-filters-module-ongoing-submission"]').click();
     // await page.getByText('Ongoing Submission', { exact: true }).click();
 
+    // await page.getByRole('button', { name: '15/01/2024 - 15/01/2027' }).click();
+
     // Click on apply button for filter
-    await page.getByRole('button', { name: 'Apply' }).click();
+    await page.locator('[data-id="hackathon-listing-filter-apply"]').click();
+    // await page.getByRole('button', { name: 'Apply' }).click();
 
     // Click on cross icon to close the filter section
     await page.locator("//div[@class='cursor-pointer border border-text-h2sGrey-300 p-1 rounded-full text-gray-500 hover:text-gray-700']//*[name()='svg']//*[name()='path' and contains(@fill,'none')]").click();
 
+    // To scroll down the page
     await page.evaluate(() => {
         window.scrollTo(0, document.body.scrollHeight);
     });
---
+++ tests/registration-form.spec.js
@@ -0,0 +1,58 @@
+const { test, expect } = require('@playwright/test');
+
+// Open URL in browser
+test('Valid Login', async ({ page }) => {
+
+    // login on development environment
+    await page.goto('https://developmentvision.hack2skill.com/login');
+
+    //   wait for 3 sec
+    await page.waitForTimeout(3000);
+
+    //   if cookie message appears then click on accept cookies button
+    const cookieBtn = page.locator('[data-id="accept-cookies"]');
+    if (await cookieBtn.isVisible({ timeout: 5000 }).catch(() => false)) {
+        await cookieBtn.click();
+        await cookieBtn.waitFor({ state: 'hidden' });
+        console.log('✅ Cookie accepted');
+    }
+
+    //   Enter emaiid to login 
+    await page.getByPlaceholder('Enter Email').fill('amar@hack2skill.com');
+
+    //   Click on login button
+    await page.locator('[data-id="auth-login-button"]').click();
+
+    //   Wait for 3 sec
+    await page.waitForTimeout(3000);
+
+    //   Enter OTP recived on entered Email id
+    // await page.locator('[data-id="auth-otp-input"][0]').fill('123456');
+    const otp = '121212';
+    const otpInputs = page.locator('[data-id="auth-otp-input"]');
+
+    for (let i = 0; i < otp.length; i++) {
+        await otpInputs.nth(i).fill(otp[i]);
+    }
+
+    //   Click on Verify buttons
+    await page.locator('[data-id="auth-verify-button"]').click();
+
+    //   Wait for 3 sec
+    await page.waitForTimeout(3000);
+
+    //   Click on profile icon on navbar after login 
+    await page.locator('[data-id="nav-profile-button"]').click();
+
+    // Click on log out button
+    // await page.locator('[data-id="nav-logout-button"]').click();
+    //   await page.locator('[data-id="nav-logout-button"]:visible').click();
+
+    // Click on innovator dashboard from navbar
+    await page.locator('[data-id="nav-menu-item-innovator-dashboard"]:visible').click();
+
+    await page.locator('div').filter({ hasText: 'Product Evolution Review' }).first()
+
+    await page.waitForTimeout(3000);
+
+});
